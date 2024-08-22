a2112a51b680193e42abacb9ac2ea4001f542bd7
Taejoon Jeong <jungtaejoon@gmail.com>
2024-08-22 15:29:01 +0000

    r

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,18 +7,4 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, '../server/public'),
   },
-  optimizeDeps: {
-    include: ['crypto-js', 'axios', 'archiver', 'cors', 'nodemailer', 'pdf-lib', 'xlsx'],
-  },
-  resolve: {
-    alias: {
-      'archiver': path.resolve(__dirname, '../node_modules/archiver'),
-      'axios': path.resolve(__dirname, '../node_modules/axios'),
-      'crypto-js': path.resolve(__dirname, '../node_modules/crypto-js'),
-      'cors': path.resolve(__dirname, '../node_modules/cors'),
-      'nodemailer': path.resolve(__dirname, '../node_modules/nodemailer'),
-      'pdf-lib': path.resolve(__dirname, '../node_modules/pdf-lib'),
-      'xlsx': path.resolve(__dirname, '../node_modules/xlsx'),
-    },
-  },
 });
