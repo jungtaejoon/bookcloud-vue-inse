e6fed642bd5c46cee02dd78fabec73baa173d52f
Taejoon Jeong <jungtaejoon@gmail.com>
2024-08-22 17:03:17 +0000

    1g23

--- server/server.js
+++ server/server.js
@@ -227,5 +227,5 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
+    console.log(`Server is running on port ${port}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
 });
